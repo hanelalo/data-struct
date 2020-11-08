6faa3ddb560ea5fa8a2f5b75d12c8be0d0ef5f74
hanelalo <hanelalo@163.com>
2020-11-08 22:31:46 +0800

    add BinarySearchTree

---
+++ src/com/hanelalo/struct/BinarySearchTree.java
@@ -0,0 +1,110 @@
+package com.hanelalo.struct;
+
+/**
+ * 二叉查找树
+ */
+public class BinarySearchTree {
+
+  private Node tree;
+
+  public Node find(int value) {
+    if (tree == null) {
+      return null;
+    }
+    Node p = tree;
+    /** 从根节点开始遍历 如果 value 比当前节点值大，向右继续查找 如果 value 比当前节点值小，向左继续查找 */
+    while (p != null) {
+      if (value < p.data) {
+        p = p.left;
+      } else if (value > p.data) {
+        p = p.right;
+      } else {
+        return p;
+      }
+    }
+    return null;
+  }
+
+  public void insert(int value) {
+    if (tree == null) {
+      tree = new Node(value);
+      return;
+    }
+    Node p = tree;
+    while (p != null) {
+      if (p.data > value) {
+        if (p.left == null) {
+          p.left = new Node(value);
+          return;
+        }
+        p = p.left;
+      } else if (p.data < value) {
+        if (p.right == null) {
+          p.right = new Node(value);
+          return;
+        }
+        p = p.right;
+      }
+    }
+  }
+
+  public void delete(int value) {
+    Node p = tree;
+    Node pp = null;
+    while (p != null && p.data != value) {
+      pp = p;
+      if (p.data > value) {
+        p = p.left;
+      } else {
+        p = p.right;
+      }
+    }
+    if (p == null) {
+      return;
+    }
+
+    // 要删除的节点有左右子节点
+    if (p.left != null && p.right != null) {
+      // 先找到 p 的右子节点中的最小值
+      Node min = p.right;
+      Node minPP = p;
+      while (min.left != null) {
+        minPP = min;
+        min = min.left;
+      }
+      p.data = min.data;
+      pp = minPP;
+      p = min;
+    }
+
+    // pp 最终的子节点
+    Node child;
+    if (p.left != null) {
+      child = p.left;
+    } else if (p.right != null) {
+      child = p.right;
+    } else {
+      child = null;
+    }
+
+    if (pp == null) {
+      // 删除根节点
+      tree = child;
+    } else if (pp.left == p) {
+      pp.left = child;
+    } else {
+      pp.right = child;
+    }
+  }
+
+  static class Node {
+
+    private int data;
+    private Node left;
+    private Node right;
+
+    public Node(int data) {
+      this.data = data;
+    }
+  }
+}
